c71cdc7800c57219b444284fd9c3ada26dab6408
Przemyslaw Pokorski <Przemyslaw_Pokorski@dell.com>
2020-12-22 18:25:20 +0000

    Changed placemark activity to update when placemark was passed

--- app/src/main/java/com/example/placemark/main/MainApp.kt
+++ app/src/main/java/com/example/placemark/main/MainApp.kt
@@ -11,9 +11,9 @@ class MainApp : Application(), AnkoLogger {
     val placemarks = PlacemarkMemStore()
 
     override fun onCreate() {
-        placemarks.create(PlacemarkModel("One", "About one..."))
-        placemarks.create(PlacemarkModel("Two", "About two..."))
-        placemarks.create(PlacemarkModel("Three", "About three..."))
+        placemarks.create(PlacemarkModel(title = "One", description = "About one..."))
+        placemarks.create(PlacemarkModel(title = "Two", description = "About two..."))
+        placemarks.create(PlacemarkModel(title = "Three", description = "About three..."))
 
         super.onCreate()
         info("Debug: Placemark main app started")
