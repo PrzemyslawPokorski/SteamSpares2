6c994ede1e99ec653c1c6c5cf4cefab3ce52d4bc
Przemyslaw Pokorski <Przemyslaw_Pokorski@dell.com>
2020-12-28 17:23:27 +0000

    Added permissions required for downloading images for given appid

--- app/src/main/java/com/example/placemark/adapters/GamelistAdapter.kt
+++ app/src/main/java/com/example/placemark/adapters/GamelistAdapter.kt
@@ -1,20 +1,27 @@
-import android.R.attr.label
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.os.AsyncTask
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.view.drawToBitmap
 import androidx.recyclerview.widget.RecyclerView
 import com.example.placemark.R
 import com.example.placemark.models.GameModel
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.card_game.*
 import kotlinx.android.synthetic.main.card_game.view.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import java.util.concurrent.Executor
 
 
 class GameListAdapter constructor(
@@ -39,13 +46,17 @@ class GameListAdapter constructor(
 
     override fun getItemCount(): Int = games.size
 
-    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class MainHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView), AnkoLogger{
         fun bind(game: GameModel, listener: GameListener) {
             itemView.gameTitle.text = game.name
             itemView.notes.text = game.notes
             itemView.codeText.text = game.code
             itemView.storeLink.text = game.url
+
+            val banner = "https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header_alt_assets_3.jpg"
+//            val bitmap: Bitmap = Picasso.get().load(banner).get()
+            Picasso.get().load(banner).into(itemView.image)
+            info { "Debug: Setting image to ${itemView.image}" }
 
             itemView.setOnClickListener {
                 TransitionManager.beginDelayedTransition(it.gameCardView, AutoTransition())
@@ -53,6 +64,10 @@ class GameListAdapter constructor(
                     it.expandableCard.visibility = View.VISIBLE
                 else
                     it.expandableCard.visibility = View.GONE
+
+                val banner = "https://cdn.cloudflare.steamstatic.com/steam/apps/${game.appid}/header_alt_assets_3.jpg"
+                Picasso.get().load(banner).into(itemView.image)
+                info { "Debug: Setting image to ${itemView.image.width}" }
             }
 
 //            itemView.setOnClickListener { listener.onGameClick(game) }
--- app/src/main/java/com/example/placemark/main/MainApp.kt
+++ app/src/main/java/com/example/placemark/main/MainApp.kt
@@ -5,13 +5,15 @@ import com.example.placemark.models.GameMemStore
 import com.example.placemark.models.GameModel
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainApp : Application(), AnkoLogger {
-
     val gameMemStore = GameMemStore()
 
     override fun onCreate() {
-        gameMemStore.create(GameModel(name = "Game1", appid = 0, code = "1111", status = false, notes = "About game1..."))
+        gameMemStore.create(GameModel(name = "Game1", appid = 457140, code = "1111", status = false, notes = "About game1..."))
         gameMemStore.create(GameModel(name = "Game2", appid = 0, code = "2222", status = false, notes = "About game2..."))
         gameMemStore.create(GameModel(name = "Game3", appid = 0, code = "3333", status = true, notes = "About game3..."))
         gameMemStore.create(GameModel(name = "Game4", appid = 0, code = "4444", status = false, notes = "About game4..."))
