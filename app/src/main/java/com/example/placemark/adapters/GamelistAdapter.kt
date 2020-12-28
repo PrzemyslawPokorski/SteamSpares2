80527be90245102107967ac78f229588f1c33a28
Przemyslaw Pokorski <Przemyslaw_Pokorski@dell.com>
2020-12-28 16:11:39 +0000

    Added game code and hyperlink to game card details

--- app/src/main/java/com/example/placemark/adapters/GamelistAdapter.kt
+++ app/src/main/java/com/example/placemark/adapters/GamelistAdapter.kt
@@ -1,9 +1,14 @@
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.transition.AutoTransition
 import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.cardview.widget.CardView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.RecyclerView
 import com.example.placemark.R
 import com.example.placemark.models.GameModel
@@ -11,10 +16,12 @@ import kotlinx.android.synthetic.main.card_game.*
 import kotlinx.android.synthetic.main.card_game.view.*
 import org.jetbrains.anko.AnkoLogger
 
+
 class GameListAdapter constructor(
     private var games: List<GameModel>,
     private val listener: GameListener
 ) : RecyclerView.Adapter<GameListAdapter.MainHolder>(), AnkoLogger {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
         return MainHolder(
             LayoutInflater.from(parent.context).inflate(
@@ -37,6 +44,9 @@ class GameListAdapter constructor(
         fun bind(game: GameModel, listener: GameListener) {
             itemView.gameTitle.text = game.name
             itemView.notes.text = game.notes
+            itemView.codeText.text = game.code
+            itemView.storeLink.text = game.url
+
             itemView.setOnClickListener {
                 TransitionManager.beginDelayedTransition(it.gameCardView, AutoTransition())
                 if(it.expandableCard.visibility == View.GONE)
@@ -44,7 +54,15 @@ class GameListAdapter constructor(
                 else
                     it.expandableCard.visibility = View.GONE
             }
-//            itemView.expandableCard.visibility = View.VISIBLE
+
+//            itemView.setOnClickListener { listener.onGameClick(game) }
+
+            itemView.codeText.setOnClickListener {
+                var clipboard = it.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                var clip = ClipData.newPlainText("label", game.code)
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(it.context, "CODE COPIED", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
